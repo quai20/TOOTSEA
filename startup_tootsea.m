ba72ef95f6d6dc86ea7adbba30f4fb8d4c35ba6e
quai20 <kevin.balem@gmail.com>
2020-04-21 12:16:24 +0200

    structure

--- startup_tootsea.m
+++ startup_tootsea.m
@@ -1,10 +1,9 @@
-function startup_req()
+function startup_tootsea()
 %
 addpath(genpath('AutoQC')); 
 addpath(genpath('Common')); 
 addpath(genpath('GUI')); 
 addpath(genpath('Parser')); 
-addpath(genpath('Science')); 
 
 %START TOOTSEA REQ
 !synclient HorizTwoFingerScroll=0
